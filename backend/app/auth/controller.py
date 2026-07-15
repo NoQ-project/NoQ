baa5cb10462610ab3b47cc52ded5e56ae6e1f9c8
Pratik Kamali <pratikkamali123@gmail.com>
2026-07-15 21:10:06 +0545

    feat: resend OTP with ratelimit and cooldown

--- backend/app/auth/controller.py
+++ backend/app/auth/controller.py
@@ -12,7 +12,7 @@ import redis
 from redis import Redis
 import secrets
 from backend.app.utils.mail import send_verification_email
-from backend.app.config.redis_client import save_pending_registration, verify_registration
+from backend.app.config.redis_client import save_pending_registration, verify_registration, store_and_send_otp, check_cooldown, start_cooldown, check_rate_limit
 
 
 password_hash = PasswordHash.recommended()
@@ -25,9 +25,11 @@ def register(body: RegisterSchema, bg_tasks:BackgroundTasks, db: Session):
     if is_user:
         raise HTTPException(status_code = status.HTTP_409_CONFLICT, detail="email already exists")
     hash_password= get_password_hash(body.password)
-    otp = save_pending_registration(body, hash_password)
-    bg_tasks.add_task(send_verification_email, body.email, otp)
-    return {"message": "verify the email now"}
+    # otp = save_pending_registration(body, hash_password)
+    save_pending_registration(body, hash_password)
+    store_and_send_otp(body.email, bg_tasks)
+    # bg_tasks.add_task(send_verification_email, body.email, otp)
+    return {"message": "OTP sent in email"}
 
 def verify_register(body:VerifyEmailSchema, db: Session):
     data=verify_registration(body)
@@ -43,23 +45,27 @@ def verify_register(body:VerifyEmailSchema, db: Session):
     db.refresh(new_user)
     return new_user
    
+async def resend_otp(body:VerifyEmailSchema, bg_tasks: BackgroundTasks):
+    if not await redis.exists(f"register:{body.email}"):
+        raise HTTPException(status_code=400, detail="Registration has expired. Please register again")
+    await check_cooldown(f"cooldown:resend_otp:{body.email}")
+    await check_rate_limit(f"rate_limit:resesnd_otp:{body.email}", 5, 60)
+    await store_and_send_otp(body.email, bg_tasks)
+    await start_cooldown(f"cooldown:resend_otp:{body.email}", 120)
+    return {"message":"OTP sent successfully"}
+
 def verify_password(plain_password, hash_password):
     return password_hash.verify(plain_password, hash_password)
 
 def login_user(body:LoginSchema, db:Session):
-    
     user = db.query(UserModel).filter(UserModel.email==body.email).first()
-    
     if not user:
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail="Invalid Email")
-    
     if not verify_password(body.password, user.password_hash):
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail="Invalid Password")
 
     exp_time = datetime.now()+ timedelta(minutes=settings.EXP_TIME)
-
     token = jwt.encode({"id":user.id,"exp":exp_time}, settings.SECRET_KEY, settings.ALGORITHM)
-    
     return {"token": token}
     
     
--- backend/app/config/redis_client.py
+++ backend/app/config/redis_client.py
@@ -4,7 +4,7 @@ import json
 import secrets
 import redis
 from backend.app.utils.mail import send_verification_email
-from fastapi import HTTPException
+from fastapi import HTTPException, BackgroundTasks
 from pydantic import BaseModel, EmailStr
 
 redis_client = Redis(
@@ -14,8 +14,8 @@ redis_client = Redis(
     decode_responses=True
 )
 
-def generate_otp():
-    return f"{secrets.randbelow(1000000):06d}"
+# def generate_otp():
+#     return f"{secrets.randbelow(1000000):06d}"
 
 def save_pending_registration(body, hashed_password):
     registration_data = {
@@ -25,22 +25,20 @@ def save_pending_registration(body, hashed_password):
         "hash_password": hashed_password,
         "role": body.role.value,
     }
-    key = f"register:{body.email}"
 
     redis_client.setex(
         f"register:{body.email}",
-        600, 
+        900, 
         json.dumps(registration_data)
     )
-    print("Saved:", key)
-    print(redis_client.get(key))
-    otp = generate_otp()
-    redis_client.setex(
-        f"verify_email:{body.email}",
-        300,
-        otp
-    )
-    return otp
+   
+    # otp = generate_otp()
+    # redis_client.setex(
+    #     f"verify_email:{body.email}",
+    #     300,
+    #     otp
+    # )
+    # return registration_data
 
 def verify_registration(body):
 
@@ -50,7 +48,7 @@ def verify_registration(body):
             status_code=400,
             detail="Registration expired."
         )
-    stored_otp= redis_client.get(f"verify_email:{body.email}")
+    stored_otp= redis_client.get(f"otp:{body.email}")
     if stored_otp is None:
         raise HTTPException(status_code=400,detail="OTP expired.")
     if  body.otp != stored_otp:
@@ -61,3 +59,31 @@ def verify_registration(body):
     redis_client.delete(f"register:{body.email}")
 
     return registration_data
+
+async def store_and_send_otp(email: str, bg_tasks: BackgroundTasks):
+    otp = f"{secrets.randbelow(1000000):06d}"
+    await redis.setex(
+        f"otp:{email}",
+        300,
+        otp
+    )
+    bg_tasks.add_task(send_verification_email, email, otp)
+
+async def start_cooldown(key:str, seconds:int):
+    await redis.setx(key, seconds,"1")
+
+async def check_cooldown(key:str):
+    ttl = await redis.ttl(key)
+    if ttl > 0:
+        raise HTTPException(status_code=429, detail= f"Please wait for {ttl} seconds" )
+
+async def check_rate_limit(key: str, limit: int, window: int):
+    count = await redis.incr(key)
+    if count == 1:
+        await redis.expire(key, window)
+    if count > limit:
+        ttl = await redis.ttl(key)
+        raise HTTPException(
+            status_code=429,
+            detail=f"Too many requests. Try again in {ttl} seconds."
+        )

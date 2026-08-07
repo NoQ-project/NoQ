62ae52e8865538bee131a3460eed93b15e02fa93
Pratik Kamali <pratikkamali123@gmail.com>
2026-08-07 11:55:36 +0545

    bugfix: settings.py

--- backend/app/utils/settings.py
+++ backend/app/utils/settings.py
@@ -8,4 +8,18 @@ class Settings(BaseSettings):
     ALGORITHM: str
     REFRESH_TOKEN_EXP_TIME:int
     ACCESS_TOKEN_EXP_TIME:int
+    MAIL_USERNAME: int
+    MAIL_PASSWORD: int
+    MAIL_FROM: int
+    MAIL_PORT: int
+    MAIL_SERVER: int
+    MAIL_FROM_NAME: int
+    MAIL_STARTTLS: int
+    MAIL_SSL_TLS: int
+    USE_CREDENTIALS: int
+    VALIDATE_CERTS: int
+    REDIS_HOST: int
+    REDIS_PORT: int
+    REDIS_DB: int
+
 settings = Settings()

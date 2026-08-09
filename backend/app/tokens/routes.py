6ccf22551bb9afb25a66a7e8129cd6c0df260e56
Bhupi Joshi <storytorch33@gmail.com>
2026-08-09 07:53:21 +0545

    Update token estimated time using service average

--- backend/app/tokens/routes.py
+++ backend/app/tokens/routes.py
@@ -2,162 +2,157 @@ from typing import List
 
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
+
 from backend.app.auth.models import UserModel
-from backend.app.auth.dependencies import get_current_user, get_owned_queue, get_owned_token
+from backend.app.auth.dependencies import (
+    get_current_user,
+    get_owned_token,
+)
 from backend.app.tokens import controller
 from backend.app.tokens.models import TokenStatus
 from backend.app.tokens.schemas import (
-    BookTokenSchema,
     TokenResponseSchema,
     TokenDetailSchema,
     WaitingPositionSchema,
     CurrentTokenSchema,
-    WaitingTokensSchema
-
+    WaitingTokensSchema,
 )
 from backend.app.utils.database import get_db
 
+
 token_routes = APIRouter(
     prefix="/tokens",
-    tags=["Tokens"]
+    tags=["Tokens"],
 )
 
 
 @token_routes.post(
     "/book",
     response_model=TokenResponseSchema,
-    status_code=status.HTTP_201_CREATED
-)
-
-@token_routes.post(
-    "/book",
-    status_code=status.HTTP_201_CREATED
+    status_code=status.HTTP_201_CREATED,
 )
 def book_token_route(
     queue_id: int,
     current_user: UserModel = Depends(get_current_user),
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
 ):
     return controller.book_token_controller(
         queue_id=queue_id,
         user_id=current_user.profile.id,
-        db=db
+        db=db,
     )
 
 
 @token_routes.post(
-    "/queues/{queue_id}/advance"
+    "/queues/{queue_id}/advance",
+    status_code=status.HTTP_200_OK,
 )
 def advance_token(
     queue_id: int,
     result: TokenStatus,
-    current_user: UserModel = Depends(),
-    db:Session = Depends(get_db)
+    current_user: UserModel = Depends(get_current_user),
+    db: Session = Depends(get_db),
 ):
     return controller.advance_token_controller(
         queue_id=queue_id,
         result=result,
-        db=db
+        db=db,
     )
+
 
 @token_routes.get(
     "/my-tokens",
     response_model=List[TokenResponseSchema],
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
 )
 def get_my_tokens(
-     current_user: UserModel = Depends(get_current_user),
-    db: Session = Depends(get_db)
+    current_user: UserModel = Depends(get_current_user),
+    db: Session = Depends(get_db),
 ):
     return controller.get_my_tokens(
         user_id=current_user.profile.id,
-        db=db
+        db=db,
     )
 
 
 @token_routes.get(
     "/{token_id}",
     response_model=TokenDetailSchema,
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
 )
 def get_token_details(
     token_id: int,
     current_user: UserModel = Depends(get_owned_token),
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
 ):
     return controller.get_token_details(
         token_id=token_id,
         user_id=current_user.profile.id,
-        db=db
+        db=db,
     )
+
 
 @token_routes.patch(
     "/{token_id}/cancel",
     response_model=TokenDetailSchema,
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
 )
 def cancel_token(
-    queue_id: int,
     token_id: int,
-     current_user: UserModel = Depends(get_owned_token),
-    db: Session = Depends(get_db)
+    queue_id: int,
+    current_user: UserModel = Depends(get_owned_token),
+    db: Session = Depends(get_db),
 ):
     return controller.cancel_token(
-        queue_id= queue_id,
+        queue_id=queue_id,
         token_id=token_id,
         user_id=current_user.profile.id,
-        db=db
+        db=db,
     )
+
 
 @token_routes.get(
     "/{token_id}/waiting-position",
     response_model=WaitingPositionSchema,
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
 )
 def get_waiting_position(
-
-    token_id:int,
-     current_user: UserModel = Depends(get_owned_token),
-    db:Session=Depends(get_db)
+    token_id: int,
+    current_user: UserModel = Depends(get_owned_token),
+    db: Session = Depends(get_db),
 ):
-
     return controller.get_waiting_position(
-
         token_id=token_id,
         user_id=current_user.profile.id,
-        db=db
+        db=db,
     )
-
 
 
 @token_routes.get(
     "/current-token/{queue_id}",
     response_model=CurrentTokenSchema,
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
 )
 def get_current_token(
     queue_id: int,
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
 ):
-
     return controller.get_current_token(
         queue_id=queue_id,
-        db=db
+        db=db,
     )
+
 
 @token_routes.get(
     "/waiting-tokens/{queue_id}",
-    response_model=list[WaitingTokensSchema],
-    status_code=status.HTTP_200_OK
+    response_model=List[WaitingTokensSchema],
+    status_code=status.HTTP_200_OK,
 )
 def get_waiting_tokens(
-
     queue_id: int,
-    db: Session = Depends(get_db)
-
+    db: Session = Depends(get_db),
 ):
-
     return controller.get_waiting_tokens(
         queue_id=queue_id,
-        db=db
+        db=db,
     )
